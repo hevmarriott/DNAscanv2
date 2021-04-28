478fdd372e7c1747981b266130a74e13d60fe32b
Heather Marriott <71556548+hevmarriott@users.noreply.github.com>
2021-04-28 17:51:51 +0100

    Update DNAscan.py

--- scripts/DNAscan.py
+++ scripts/DNAscan.py
@@ -557,7 +557,7 @@ def is_variant_file_OK(file, t):
                 f.close()
             elif t == "Vcf":
                 f = pysam.VariantFile(file, 'r')
-                for r in f.fetch(str(chr), int(start), int(end):
+                for r in f.fetch(str(chr), int(start), int(end)):
                     empty = next(f.fetch(chr,start,end), None) is None
                     if empty:
                         sys.exit("\nWARNING: %s only contains the header and no data, therefore DNAscan will now terminate.\n" % file)
