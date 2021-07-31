40506f7677a0b8cdefeef3338ee3de0fe250eaf0
Heather Marriott <71556548+hevmarriott@users.noreply.github.com>
2021-07-31 16:07:46 +0100

    Update paths_configs.py

--- scripts/paths_configs.py
+++ scripts/paths_configs.py
@@ -55,9 +55,9 @@ path_knotannotsv = ""
 #how to download the annovar databases and to modify annovar_protocols and annovar_operations please visit 
 #http://annovar.openbioinformatics.org/en/latest/ . 
 
-annovar_protocols = "refGene,dbnsfp33a,clinvar_20210501,avsnp147,exac03"
+annovar_protocols = "refGene,dbnsfp33a,clinvar_20210501,avsnp147,exac03,intervar_20180118"
 
-annovar_operations = "g,f,f,f,f"
+annovar_operations = "g,f,f,f,f,f"
 
 path_expansionHunter = ""
 
