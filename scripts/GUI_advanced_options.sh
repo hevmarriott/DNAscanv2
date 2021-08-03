bfdaca8a8f0b88d457576baaa68add489b8c50a1
Heather Marriott <71556548+hevmarriott@users.noreply.github.com>
2021-08-03 23:17:17 +0100

    Create GUI_advanced_options.sh

---
+++ scripts/GUI_advanced_options.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Usage: bash add_dependencies.sh $path_to_gene_list $path_to_bed_file $hisat_options $bwa_options $annotsv_options $melt_options
+
+path_gene_list=$1
+
+path_bed=$2
+
+hisat_custom_options=$3
+
+bwa_custom_options=$4
+
+annotsv_custom_options=$5
+
+melt_custom_options=$6
+
+sed "s|path_gene_list = \"\"|path_gene_list = \"$path_gene_list\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
+
+sed "s|path_bed = \"\"|path_bed = \"$path_bed\"|" scripts/paths_configs.py_temp > scripts/paths_configs.py
+
+sed "s|hisat_custom_options = \"\"|hisat_custom_options = \"$hisat_custom_options\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
+
+sed "s|bwa_custom_options = \"\"|bwa_custom_options = \"$bwa_custom_options\"|" scripts/paths_configs.py_temp > scripts/paths_configs.py
+
+sed "s|annotsv_custom_options = \"\"|annotsv_custom_options = \"$annotsv_custom_options\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
+
+sed "s|melt_custom_options = \"\"|melt_custom_options = \"$melt_custom_options\"|" scripts/paths_configs.py_temp > scripts/paths_configs.py
+
+rm scripts/paths_configs.py_temp
