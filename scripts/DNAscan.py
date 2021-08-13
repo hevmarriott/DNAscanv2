b6b56244e142e0d415ae0ab33e649a53704dbae3
Heather Marriott <71556548+hevmarriott@users.noreply.github.com>
2021-08-13 17:06:42 +0100

    Update DNAscan.py

--- scripts/DNAscan.py
+++ scripts/DNAscan.py
@@ -54,6 +54,7 @@
 # 21. Html report generation ( Multiqc )
 # 22. Annotated variants results report generation
 #   22.1 knotAnnotSV annotated structural variants report generation
+#   22.2 Concise results report for all variants called (SV, MEI, SNVs and indels)
 # 23. Starting iobio services
 #################################################################
 
@@ -1598,7 +1599,7 @@ if results_report:
                     
             #22.2 Concise results report for all variants called (SV, MEI, SNVs and indels)
             if os.path.isfile("%s/reports/%s_annovar_variants.txt" % out, sample_name) == True:
-                if SV_results_file == "%s/results/%s_manta_SV.vcf.gz" % (out, sample_name) or if not SV and os.path.isfile(MEI_results_file) == True: 
+                if SV_results_file == "%s/results/%s_manta_SV.vcf.gz" % (out, sample_name) or not SV and os.path.isfile(MEI_results_file) == True: 
                     if mode == "fast" and os.path.isfile(SV_annotation_file) == True:
                         annotsv_file = SV_annotation_file
                 
