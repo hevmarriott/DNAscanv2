ac0fc8f0f2cd8dd371c023a4c006501ab6df1b5a
Heather Marriott <71556548+hevmarriott@users.noreply.github.com>
2021-08-27 12:39:56 +0100

    Update GUI_add_read_group.sh

--- scripts/GUI_add_read_group.sh
+++ scripts/GUI_add_read_group.sh
@@ -12,16 +12,14 @@ RG_PU=$4
 
 RG_SM=$5
 
-sed "s|RG_ID = \"\"|RG_ID = \"$RG_ID\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
+sed -i "s|RG_ID = \"\"|RG_ID = \"$RG_ID\"|" scripts/paths_configs.py 
 
-sed "s|RG_LB = \"\"|RG_LB = \"$RG_LB\"|" scripts/paths_configs.py_temp > scripts/paths_configs.py
+sed -i "s|RG_LB = \"\"|RG_LB = \"$RG_LB\"|" scripts/paths_configs.py
 
-sed "s|RG_SM = \"\"|RG_SM = \"$RG_SM\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
+sed -i "s|RG_SM = \"\"|RG_SM = \"$RG_SM\"|" scripts/paths_configs.py 
 
-sed "s|RG_PU = \"\"|RG_PU = \"$RG_PU\"|" scripts/paths_configs.py_temp > scripts/paths_configs.py
+sed -i "s|RG_PU = \"\"|RG_PU = \"$RG_PU\"|" scripts/paths_configs.py
 
-sed "s|RG_PL = \"\"|RG_PL = \"$RG_PL\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
-
-mv scripts/paths_configs.py_temp scripts/paths_configs.py
+sed -i "s|RG_PL = \"\"|RG_PL = \"$RG_PL\"|" scripts/paths_configs.py 
 
 chmod +x scripts/*
