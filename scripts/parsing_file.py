82849e5f3c7b59b54f63a189c3b35dbdf5311295
Heather Marriott <71556548+hevmarriott@users.noreply.github.com>
2023-01-08 11:47:10 +0000

    Update parsing_file.py

--- scripts/parsing_file.py
+++ scripts/parsing_file.py
@@ -76,8 +76,8 @@ def create_parser():
       action="store_true",
       dest="fast_mode",
       help=
-      'if this flag is set DNAscan2 will run without SV calling with Delly and the genotyping of STR loci identified with ExpansionHunter Denovo (Default = "True")',
-      default=True)
+      'if this flag is set DNAscan2 will run without SV calling with Delly and the genotyping of STR loci identified with ExpansionHunter Denovo if those flags are set (Default = "False")',
+      default=False)
 
   parser.add_argument(
       '-alignment',
