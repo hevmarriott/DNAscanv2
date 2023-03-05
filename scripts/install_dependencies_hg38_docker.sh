c2807130346eefb6090d0b1e57361ac86f5616e9
Heather Marriott <71556548+hevmarriott@users.noreply.github.com>
2023-03-05 19:37:25 +0000

    Update install_dependencies_hg38_docker.sh

--- scripts/install_dependencies_hg38_docker.sh
+++ scripts/install_dependencies_hg38_docker.sh
@@ -11,7 +11,7 @@ apt-get install -y update
 
 apt-get install -y vim
 
-apt-get install -y python3
+apt-get install -y python2
 
 apt-get install -y perl
 
@@ -19,9 +19,13 @@ apt-get install -y wget bzip2
 
 apt-get install -y ttf-dejavu
 
+apt-get install -y gzip make git tcl tcllib tar gcc g++
+
 mkdir $INSTALL_DIR
 
 mkdir $INSTALL_DIR/humandb
+
+cd $DNASCAN_DIR
 
 cd $INSTALL_DIR
 
@@ -47,33 +51,45 @@ conda install -y python=3.8.0
 
 conda install -y pysam=0.16.0.1
 
-conda install -y samtools=1.10
+conda install -y samtools=1.9
 
-conda install -y freebayes>=1.0.2
+conda install -y bedtools=2.25.0
 
-conda install -y bedtools>=2.25.0
+conda install -y sambamba=0.7.1
 
-conda install -y vcftools>=0.1.13
+conda install -y samblaster=0.1.26
 
-conda install -y bcftools=1.10.2
+conda install -y vcftools=0.1.16
 
-conda install -y hisat2>=2.1.0
+conda install -y bcftools=1.9
+
+conda install -y hisat2=2.2.1
 
 conda install -y bwa=0.7.17
 
 conda install -y rtg-tools=3.12
 
-conda install -y multiqc>=1.2
+conda install -y multiqc=1.10.1
 
 conda install -y fastqc=0.11.9
 
-conda install -y expansionhunter=4.0.2
+conda install -y expansionhunter=3.2.2
 
-conda install -y wham=1.8.0
+conda install -y delly=0.8.3
 
-conda install -y sambamba>=0.6.6
+conda install -y pysimplegui=4.60.4
 
-conda install -y samblaster>=0.1.24
+conda install -y survivor=1.0.7
+
+conda install -y perl=5.26.2=h470a237_0
+
+conda install -y biopython=1.78
+
+conda install -y perl-app-cpanminus
+
+cpan YAML::XS
+
+cpan Sort::Key::Natural
 
 cd $DNASCAN_DIR
 
@@ -97,9 +113,13 @@ apt-get install -y -qq bzip2 gcc g++ make python zlib1g-dev
 
 cd $INSTALL_DIR
 
-mkdir manta
+wget https://github.com/Illumina/strelka/releases/download/v2.9.10/strelka-2.9.10.centos6_x86_64.tar.bz2
 
-cd manta
+tar -xjf strelka-2.9.10.centos6_x86_64.tar.bz2
+
+export PATH=$INSTALL_DIR/strelka-2.9.10.centos6_x86_64/bin:$PATH
+
+echo export PATH=$INSTALL_DIR/strelka-2.9.10.centos6_x86_64/bin:$PATH >> ~/.bashrc
 
 wget https://github.com/Illumina/manta/releases/download/v1.6.0/manta-1.6.0.centos6_x86_64.tar.bz2
 
@@ -109,6 +129,36 @@ export PATH=$INSTALL_DIR/manta-1.6.0.centos6_x86_64/bin:$PATH
 
 echo export PATH=$INSTALL_DIR/manta-1.6.0.centos6_x86_64/bin:$PATH >> ~/.bashrc
 
+wget https://github.com/Illumina/ExpansionHunterDenovo/releases/download/v0.9.0/ExpansionHunterDenovo-v0.9.0-linux_x86_64.tar.gz
+
+tar -xzf ExpansionHunterDenovo-v0.9.0-linux_x86_64.tar.gz
+
+export PATH=$INSTALL_DIR/ExpansionHunterDenovo-v0.9.0-linux_x86_64/:$PATH
+
+echo export PATH=$INSTALL_DIR/ExpansionHunterDenovo-v0.9.0-linux_x86_64/:$PATH >> ~/.bashrc
+
+git clone https://github.com/lgmgeo/AnnotSV
+
+cd AnnotSV
+
+make PREFIX=. install
+
+make PREFIX=. install-human-annotation
+
+export ANNOTSV=$INSTALL_DIR/AnnotSV/
+
+cd $ANNOTSV/share/AnnotSV/Annotations_Human/Genes/GRCh37
+
+wget https://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/refGene.txt.gz
+
+cd $INSTALL_DIR
+
+git clone https://github.com/mobidic/knotAnnotSV
+
+export PATH=$INSTALL_DIR/knotAnnotSV/:$PATH
+
+echo export PATH=$INSTALL_DIR/knotAnnotSV/:$PATH >> ~/.bashrc
+
 cd $DNASCAN_DIR
 
 sed "s|path_reference = \"\"|path_reference = \"$DNASCAN_DIR\/hg38\/hg38.fa\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
@@ -116,6 +166,26 @@ sed "s|path_reference = \"\"|path_reference = \"$DNASCAN_DIR\/hg38\/hg38.fa\"|" 
 sed "s|path_hisat_index = \"\"|path_hisat_index = \"$DNASCAN_DIR\/hg38\/hg38\"|" scripts/paths_configs.py_temp > scripts/paths_configs.py
 
 sed "s|path_bwa_index = \"\"|path_bwa_index = \"$DNASCAN_DIR\/hg38\/hg38.fa\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
+
+sed "s|path_annovar_db = \"\"|path_annovar_db = \"$INSTALL_DIR\/humandb\/\"|" scripts/paths_configs.py_temp > scripts/paths_configs.py
+
+sed "s|dnascan_dir = \"\"|dnascan_dir = \"$DNASCAN_DIR\/\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
+
+sed "s|path_manta = \"\"|path_manta = \"$INSTALL_DIR\/manta-1.6.0.centos6_x86_64\/bin\/\"|" scripts/paths_configs.py_temp > scripts/paths_configs.py
+
+sed "s|path_expansionHunter_jsons = \"\"|path_expansionHunter_jsons = \"$DNASCAN_DIR\/repeats\/hg19\/\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
+
+sed "s|path_to_db = \"\"|path_to_db = \"$DNASCAN_DIR\/db\/\"|" scripts/paths_configs.py_temp > scripts/paths_configs.py
+
+sed "s|path_annotsv = \"\"|path_annotsv = \"$INSTALL_DIR\/AnnotSV\/\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
+
+sed "s|path_knotannotsv = \"\"|path_knotannotsv = \"$INSTALL_DIR\/knotAnnotSV\/\"|" scripts/paths_configs.py_temp > scripts/paths_configs.py
+
+sed "s|path_strelka = \"\"|path_strelka = \"$INSTALL_DIR\/strelka-2.9.10.centos6_x86_64\/bin\/\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
+
+sed "s|path_delly_exclude_regions = \"\"|path_delly_exclude_regions = \"$DNASCAN_DIR\/db\/delly_hg19.excl.tsv\"|"  scripts/paths_configs.py_temp > scripts/paths_configs.py
+
+sed "s|path_expansionHunterDenovo_dir = \"\"|path_expansionHunterDenovo_dir = \"$INSTALL_DIR\/ExpansionHunterDenovo-v0.9.0-linux_x86_64\/\"|" scripts/paths_configs.py > scripts/paths_configs.py_temp
 
 mv scripts/paths_configs.py_temp scripts/paths_configs.py
 
